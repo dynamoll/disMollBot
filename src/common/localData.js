3eb74a85c4086f958944454115218bbb754f1efc
Moll <ruru.50fi@gmail.com>
2023-02-06 22:15:09 +0900

    localData.jsを追加

---
+++ src/common/localData.js
@@ -0,0 +1,7 @@
+// ※各自のサーバーID、チャンネルID、DiscordBotトークンを配置してください。
+// 利用するサーバーId
+exports.serverId = "各自のサーバーIDをここへ";
+// 送信するチャンネルId
+exports.channelId = "各自のチャンネルIDをここへ";
+// Discord Bot Token
+exports.discordBotToken = "各自のDiscordBotトークンをここへ";
